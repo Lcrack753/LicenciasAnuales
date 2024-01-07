7d3e26a02c49106c79d228a87ef965f961fc78c1
Lucio <144396772+Lcrack753@users.noreply.github.com>
2024-01-07 20:12:55 -0300

    prettier datacode

--- defs/defs_data.py
+++ defs/defs_data.py
@@ -1,46 +1,42 @@
 import sqlite3
 
-conn = sqlite3.connect('./data/dataBase.db')
+def connect_to_database(database_path):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+    return conn, cursor
 
-c = conn.cursor()
+def create_tables(conn, cursor):
+    with conn:
+        # TABLE AGENT
+        cursor.execute("""CREATE TABLE agent (
+                    id INTEGER PRIMARY KEY,
+                    cuil TEXT,
+                    first TEXT,
+                    last TEXT,
+                    admission TEXT
+                    )""")
+
+        # TABLE LICENSES
+        cursor.execute("""CREATE TABLE licenses (
+                    id INTEGER PRIMARY KEY,
+                    cuil TEXT,
+                    start TEXT,
+                    end TEXT,
+                    note TEXT
+                    )""")
+
+        # TABLE days_left
+        cursor.execute("""CREATE TABLE days_left (
+                    id INTEGER PRIMARY KEY,
+                    cuil TEXT,
+                    year_2022 INTEGER,
+                    year_2023 INTEGER,
+                    year_2024 INTEGER,
+                    year_2025 INTEGER,
+                    year_2026 INTEGER
+                    )""")
+
+def close_connection(conn):
+    conn.close()
 
 
-# TABLE AGENT
-# c.execute("""CREATE TABLE agent (
-#             id INTEGRER PRIMARY KEY,
-#             cuil TEXT,
-#             first TEXT,
-#             last TEXT,
-#             admission TEXT
-#             )""")
-
-# TABLE LICENSES
-# c.execute("""CREATE TABLE licenses (
-#             id INTEGRER PRIMARY KEY,
-#             cuil TEXT,
-#             start TEXT,
-#             end TEXT,
-#             note TEXT
-#             )""")
-
-# TABLE days_left
-# c.execute("""CREATE TABLE days_left (
-#             id INTEGER PRIMARY KEY,
-#             cuil TEXT,
-#             year_2022 INTEGER,
-#             year_2023 INTEGER,
-#             year_2024 INTEGER,
-#             year_2025 INTEGER,
-#             year_2026 INTEGER
-#             )""")
-
-
-# c.execute("INSERT INTO employees VALUES ('Corey','Sanchez', 5000)")
-
-# c.execute("SELECT * FROM employees WHERE last='Sanchez'")
-
-# print(c.fetchone())
-
-conn.commit()
-
-conn.close()
